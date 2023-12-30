85cdfc1c4ae7a02a19de33452b9c00cdcb52c68e
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2023-12-30 22:05:33 +0100

    audio werkend afbeelding werkend

--- ata_trial_functions.py
+++ ata_trial_functions.py
@@ -1,15 +1,14 @@
 import openai
 from openai import OpenAI
 
-def genereer_afbeelding(apik, vraagtekst):
+def genereer_afbeelding(apik, invoer):
     client = OpenAI(api_key=apik)
-    invoer = vraagtekst
     response = client.images.generate(
-    model="dall-e-3",
-    prompt=invoer,
-    n=1,
-    size="1792x1024",
-    quality="hd"
+        model="dall-e-3",
+        prompt=invoer,
+        n=1,
+        size="1792x1024",
+        quality="hd"
     )
     return response.data[0].url
 
