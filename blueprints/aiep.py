1f61d55fa84c6993c3b49afc9e27c4cb3faa228d
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-10-18 14:12:11 +0200

    eerste spraak trial

--- blueprints/aiep.py
+++ blueprints/aiep.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from twilio.twiml.voice_response import VoiceResponse
 
 # Maak een Blueprint aan
 ai_bp = Blueprint('ai', __name__)
@@ -8,3 +9,11 @@ from ai import aifuncties as aif
 @ai_bp.route("/aitest")
 def lvs_allestudenten():
   return "aitest"
+
+
+@ai_bp.route("/voice", methods=['POST'])
+def voice_reply():
+    resp = VoiceResponse()
+    # Geef een gesproken boodschap terug
+    resp.say("Hallo! Bedankt dat je belt. Hoe kan ik je helpen?", voice='alice')
+    return str(resp)
